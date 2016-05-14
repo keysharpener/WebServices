01ba71b7ee3cec39ca365ca1f295cefeda379cda
Robin Francillon <robin.francillon@gmail.com>
2016-05-14 13:39:13 +0200

    Retrieve product name, price and currency

---
+++ WebServices.Tests/AmazonURLProductParserTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using NFluent;
+using NUnit.Framework;
+
+namespace WebServices.Tests
+{
+    [TestFixture]
+    public class AmazonURLProductParserTests
+    {
+        [Test]
+        public void Should_return_product_name_for_random_page()
+        {
+            var productWebPage =
+                @"https://www.amazon.fr/test-exp%C3%A9rience-inou%C3%AFe-preuve-lapr%C3%A8s-vie/dp/2226319085/ref=sr_1_1?ie=UTF8&qid=1463219510&sr=8-1&keywords=test";
+            var urlParser = new URLParser();
+            var amazonParser = new AmazonParser(urlParser, productWebPage);
+            Check.That(amazonParser.GetProductName()).Equals("Le test : Une expérience inouïe, la preuve de l'après-vie ?");
+        }
+
+        [Test]
+        public void Should_return_product_amount_for_random_page()
+        {
+            var productWebPage =
+                @"https://www.amazon.fr/test-exp%C3%A9rience-inou%C3%AFe-preuve-lapr%C3%A8s-vie/dp/2226319085/ref=sr_1_1?ie=UTF8&qid=1463219510&sr=8-1&keywords=test";
+            var urlParser = new URLParser();
+            var amazonParser = new AmazonParser(urlParser, productWebPage);
+            Console.WriteLine(amazonParser.GetProductPrice());
+            Check.That(amazonParser.GetProductPrice()).Equals("19,50");
+            Check.That(amazonParser.GetProductCurrency().Equals("EUR"));
+        }
+    }
+}
--- WebServices/Class1.cs
+++ WebServices/Class1.cs
@@ -1,13 +1,78 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
+using HtmlAgilityPack;
 
 namespace WebServices
 {
-    public class Class1
+    public interface IURLParser
     {
+    }
 
+    public class URLParser : IURLParser
+    {
+        private string _url;
+
+
+        public string ParseWebPage(string url)
+        {
+            using (WebClient client = new WebClient())
+            {
+                return client.DownloadString(url);
+            }
+        }
+    }
+
+    public interface IUrlParser
+    {
+    }
+
+    public class AmazonParser : IUrlParser
+    {
+        private HtmlDocument _htmlDocument;
+        private string PriceId = "//span[@class='a-size-medium a-color-price offer-price a-text-normal']";
+        private string ProductNameId = "productTitle";
+
+        public AmazonParser(URLParser parser, string url)
+        {
+            _htmlDocument = new HtmlDocument();
+            _htmlDocument.LoadHtml(parser.ParseWebPage(url));
+        }
+
+        public string GetProductName()
+        {
+            var node = _htmlDocument.GetElementbyId(ProductNameId);
+            if (node != null)
+                return HtmlEntity.DeEntitize(node.InnerText);
+            return null;
+        }
+
+        public string GetProductPrice()
+        {
+            return ParsePriceTag(1);
+        }
+
+        private string ParsePriceTag(int parameterPosition)
+        {
+            var innerText = _htmlDocument.DocumentNode.SelectSingleNode(PriceId).InnerText;
+            return HtmlEntity.DeEntitize(innerText.Split(' ')[parameterPosition]);
+        }
+
+        public string GetProductPicture()
+        {
+            return string.Empty;
+
+        }
+
+        public string GetProductCurrency()
+        {
+            return ParsePriceTag(0);
+        }
     }
 }
