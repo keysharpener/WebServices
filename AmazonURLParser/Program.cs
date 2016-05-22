a438039999530c11daa41f0d2aa5558136ab214b
= <=>
2016-05-23 00:11:13 +0200

    added basic console app

---
+++ AmazonURLParser/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using WebServices;
+
+namespace AmazonURLParser
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			NewMethod ();
+
+		}
+
+		static void NewMethod ()
+		{
+			var parser = ParseAmazonURLInfo ();
+			DisplayAmazonURLInfo (parser);
+			NewMethod ();
+		}
+
+		static AmazonParser ParseAmazonURLInfo ()
+		{
+			Console.WriteLine ("Please input the URL you want to test");
+			var url = Console.ReadLine ();
+			var urlParser = new UrlParser ();
+			var amazonParser = new AmazonParser (urlParser, url);
+			return amazonParser;
+		}
+
+		static void DisplayAmazonURLInfo (AmazonParser amazonParser)
+		{
+			Console.WriteLine(string.Format("{0}, {1} {2}", amazonParser.GetProductName(), amazonParser.GetProductPrice(), amazonParser.GetProductCurrency()));
+		}
+	}
+}
